57219178092a27e34961971ae005897ec26d3622
cristiansilveira23 <cristiansilveira195@gmail.com>
2024-05-08 21:44:41 -0300

    Aula 02 de java

---
+++ js/Js02-variaveis/exerciciocasa/main.js
@@ -0,0 +1,51 @@
+// Objeto console
+console.log("olá mundo");
+// Quando trabalho com texto coloco o mesmo dentro de aspas duplas ou simples
+
+
+// variaveis
+// quero cria um espaço na memoria do computador chamado _felippe e dizer que esse espaço na memoria irá receber o valor 8
+
+var _oi = 8;
+_oi ="Messi"
+
+let lestsGo = 4
+lestGo = 3
+console.log(lestsGo)
+
+console.log(_oi);
+
+console.clear()
+
+const PI = 3.1415
+
+console.log(PI)
+
+
+let NAN = 0 / 0
+
+console.log(NAN)
+
+
+console.clear()
+
+let IOS = "Instituto da opurtunidade social"
+
+console.log("Instituto "  + "da " + "oportunidade " + "social")
+
+var numero1 = 2
+var texto = " Eu tenho "
+var texto2 = " reais"
+
+console.log(texto + numero1 + texto2)
+
+console.clear
+
+var num1 = 1
+var num2 = 1
+
+console.log(num1 == num2)
+
+var teste
+
+console.log ()
