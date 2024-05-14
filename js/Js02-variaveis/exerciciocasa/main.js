49e62db53856b2f7681c280b58c240bc1140594c
cristiansilveira23 <cristiansilveira195@gmail.com>
2024-05-13 21:44:58 -0300

    aula 3

--- js/Js02-variaveis/exerciciocasa/main.js
+++ js/Js02-variaveis/exerciciocasa/main.js
@@ -1,96 +1,10 @@
-// Objeto console
-console.log("olá mundo");
-// Quando trabalho com texto coloco o mesmo dentro de aspas duplas ou simples
+var prova1 = 3
+var prova2 = 8
+var prova3 = 5
+var prova4 = 9
 
+var soma = prova1 + prova2 + prova3 + prova4
 
-// variaveis
-// quero cria um espaço na memoria do computador chamado _felippe e dizer que esse espaço na memoria irá receber o valor 8
+var media = soma / 4
 
-var _oi = 8;
-_oi ="Messi"
-
-let lestsGo = 4
-lestGo = 3
-console.log(lestsGo)
-
-console.log(_oi);
-
-console.clear()
-
-const PI = 3.1415
-
-console.log(PI)
-
-
-let NAN = 0 / 0
-
-console.log(NAN)
-
-
-console.clear()
-
-let IOS = "Instituto da opurtunidade social"
-
-console.log("Instituto "  + "da " + "oportunidade " + "social")
-
-var numero1 = 2
-var texto = " Eu tenho "
-var texto2 = " reais"
-
-console.log(texto + numero1 + texto2)
-
-console.clear
-
-var num1 = 1
-var num2 = 1
-
-console.log(num1 == num2)
-
-var teste
-
-console.log ()
-
-console.clear()
-
-var operador1 = 8
-var operador2 = null
-
-console.log(operador1 * operador2)
-
-var operador3 = "5"
-var operador4 = 1
-
-console.log(operador3 - operador4)
-
-var operador5 = "5"
-var operador6 = 1
-
-console.log(operador5 + operador6)
-
-var operador7 = "five"
-var operador8 = 2
-
-console.log(operador7 * operador8)
-
-console.clear()
-
-var valor1 = 2
-var valor2 = 2
-var resultado = valor1 % valor2
-
-console.log(resultado)
-
-console.clear()
-
-var compara  = "2" != 3
-
-console.log(compara);
-
-console.clear()
-
-
-let valor3 = 4
-let valor4 = 6
-let resultado1 = valor3 < valor4 || valor3 == valor4
-
-console.log(resultado1)
+console.log("A média é:" + media)
---
+++ js/js03-string/main.js
@@ -0,0 +1,65 @@
+var string = "Os alunos estão "
+var string2 = "reprovados "
+var string3 = "ou aprovados?"
+var num1 = 2
+var num2 = 4
+
+
+
+console.log(string + string2 + string3 + (num1 * num2))
+
+
+console.clear()
+
+var texto1 = "Ios fez uma excursão na TOTVS com três alunos"
+
+console.log(texto1.charAt(27))
+console.log(texto1[27])
+
+console.clear()
+
+var texto2 = "Ios"
+var texto3 = "Está chovendo hamburguer"
+
+console.log(`na variavel texto2 tem ${texto2. length}caracteres`)
+
+console.log(`na variavel texto3 tem ${texto3. length} caracteres`)
+
+console.clear()
+
+var texto4 = "aqui está em minusculo"
+var texto5 = "AQUI ESTÁ EM MAIUSCULO"
+
+console.log(`Minusculo: ${texto5.toLowerCase()}`)
+
+console.log(`Maiusculo: ${texto4.toUpperCase()}`)
+
+var nome = "PKNinaDAZL"
+
+console.log(nome.toLocaleLowerCase())
+
+console.clear()
+
+var str = "Mozilla"
+
+console.log(str)
+console.log(str.substring(1, 4))
+
+console.log(str.substring(3))
+
+
+console.clear()
+
+var texto6 ="Olha a pipoca"
+console.log(texto6)
+console.log(texto6.replace(`pipoca`, `Carreta Furacão`))
+
+console.clear()
+
+var texto7 = "           Olha a Carreta Furacão               "
+
+console.log(texto7)
+
+console.log(texto7.trim(
+    
+))
